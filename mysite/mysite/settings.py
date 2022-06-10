17a70c485525d831b9374eaf98516dcc007c1816
Andrew Choi <choiandrew585@gmail.com>
2022-06-10 16:02:13 +0900

    added gitignore file with local .env file to hide important env variables (secret key)

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5vx6(9=#y9@fl(1xoo-)x7w7$h0zpym)dszq62!!1r-3phr)gr'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
