694f0f09463e9fa8120d89a46ca00ca63cf44cef
Andrew Choi <choiandrew585@gmail.com>
2022-06-14 10:57:49 +0900

    changed views to be generic changed urls in previous commit to mirror this change

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,31 @@
 # Create your views here.
 
-from django.http import HttpResponse, HttpResponseRedirect
-from django.http import Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 #from django.template import loader
 
 from .models import Question, Choice
 from .models import Patient
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
