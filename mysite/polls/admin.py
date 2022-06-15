8323281664d3e6e40422d608f4e603e94370f428
Andrew Choi <choiandrew585@gmail.com>
2022-06-15 17:36:15 +0900

    changed layout of admin question view

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question, Choice
+from .models import Question
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
